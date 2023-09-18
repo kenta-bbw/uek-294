13a1caefc4355dcef77e988e24a7c105e032d21e
k3ntaw <kenta.waibel@gmail.com>
2023-09-18 12:47:06 +0200

    init: initializing aufgabe 1.2 , 1.3, 1.4 | by Kenta Waibel

---
+++ auftrag-1.2/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',() =>  {
+var gruessenButton = document.getElementById('gruessenButton');
+gruessenButton.onclick = () => {alert("Hallo Welt!")}
+})
---
+++ auftrag-1.4/index.js
@@ -0,0 +1,22 @@
+const fizzbuzzButton = document.getElementById("fizzbuzzButton");
+const outputDiv = document.getElementById("output");
+
+const Fizzbuzz = () => {
+  let outputText = ""; 
+
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      outputText += "FizzBuzz<br>";
+    } else if (i % 5 === 0) {
+      outputText += "Buzz<br>";
+    } else if (i % 3 === 0) {
+      outputText += "Fizz<br>";
+    } else {
+      outputText += i + "<br>";
+    }
+  }
+
+  outputDiv.innerHTML = outputText;
+};
+
+fizzbuzzButton.onclick = Fizzbuzz;
